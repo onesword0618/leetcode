93bcd19dbd29c13f79eadd31fd700534913ff206
onesword0618 <ao.akua.leo@gmail.com>
2020-01-01 21:41:28 +0900

    Update: テストコードを見直す
    二重引用符になっているところも含めて修正

--- src/test/longestCommonPrefix.test.js
+++ src/test/longestCommonPrefix.test.js
@@ -3,10 +3,10 @@ const longestCommonPrefix = require('./../main/longestCommonPrefix');
 describe('共通のプレフィックスを見つけたらその箇所を返す', () => {
 
     test('共通のプレフィックス fl を返す', () => {
-        expect(longestCommonPrefix(["flower", "flow", "flight"])).toBe("fl");
+        expect(longestCommonPrefix(['flower', 'flow', 'flight'])).toBe('fl');
     });
 
-    test('共通のプレフィックスが存在しない', () => {
-        expect(longestCommonPrefix(["dog", "racecar", "car"])).toBe("");
+    test('共通のプレフィックスが存在しない場合は空文字を返す', () => {
+        expect(longestCommonPrefix(['dog', 'racecar', 'car'])).toBe('');
     });
 });
