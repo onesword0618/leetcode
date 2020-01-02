17170451b179d80a0c50e1e82ebe40bf5dc36b62
onesword0618 <ao.akua.leo@gmail.com>
2020-01-02 23:35:04 +0900

    Update: 括弧が連続したペアの入力であるかの確認
    連続して開いているのと閉じているのペアで入力されているかの確認を行う。

---
+++ src/main/isValid.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} str
+ * @return {boolean}
+ */
+const isValid = function (str) {
+
+    if (str === null || str.length <= 0) {
+        return true;
+    }
+
+    let brackets = str.split('');
+    let stack = [];
+    for (let bracket of brackets) {
+        if (bracket === '[') {
+            stack.push(']');
+        } else if (bracket === '{') {
+            stack.push('}');
+        } else if (bracket === '(') {
+            stack.push(')');
+        } else if (stack.length === 0 || bracket !== stack.pop()) {
+            return false;
+        };
+    };
+
+    if (stack.length === 0) {
+        return true;
+    };
+    return false;
+};
+module.exports = isValid;
