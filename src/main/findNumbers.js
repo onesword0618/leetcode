e2363f5abec79fb3b0ee3e4b44be7cffdc9fd7dc
onesword0618 <ao.akua.leo@gmail.com>
2020-01-05 20:00:55 +0900

    Add: Mapとreduceを利用して問題を解く
    リファレンスを精読した

---
+++ src/main/findNumbers.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findNumbers = function (nums) {
+    return nums.map(String).reduce((previousValue, currentValue) => {
+        return previousValue + (!(currentValue.length & 1) ? 1 : 0);
+    }, 0);
+};
+module.exports = findNumbers;
