c977774f5e2fe2e9445626cfa16e372d1f564ee0
onesword0618 <ao.akua.leo@gmail.com>
2020-01-12 00:08:42 +0900

    Add: 簡単な解決方法を取り組む
    多分これ、文字コードとかの計算をするのを期待している問題のようだ。

---
+++ src/test/toLowerCase.test.js
@@ -0,0 +1,15 @@
+const toLowerCase = require('./../main/toLowerCase');
+
+describe('与えられた文字列の大文字を小文字に変換して返却する。', () => {
+    test('Helloを渡してhelloが返却される', () => {
+        expect(toLowerCase('Hello')).toBe('hello');
+    });
+
+    test('hereを渡してhereが返却される。', () => {
+        expect(toLowerCase('here')).toBe('here');
+    });
+
+    test('LOVELYを渡してlovelyが返却される。', () => {
+        expect(toLowerCase('LOVELY')).toBe('lovely');
+    });
+});
