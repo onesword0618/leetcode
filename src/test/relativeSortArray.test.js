b80b131deccd46e6fe7a019ba85a0c5c91b1ab6f
onesword0618 <ao.akua.leo@gmail.com>
2020-01-31 00:10:12 +0900

    Add: 配列の一致のテストコードを書く

---
+++ src/test/relativeSortArray.test.js
@@ -0,0 +1,9 @@
+const relativeSortArray = require('./../main/relativeSortArray');
+
+describe('第1引数の配列に対して第2引数の配列と関連している数値をまとめる。', () => {
+
+    test('[2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19]が返却される', () => {
+        expect(relativeSortArray([2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19], [2, 1, 4, 3, 9, 6]))
+            .toEqual(expect.arrayContaining([2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19]));
+    });
+});
