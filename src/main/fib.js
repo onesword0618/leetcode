add761d23fa2475a7ad2f8d34231213ddd2b9442
onesword0618 <ao.akua.leo@gmail.com>
2020-02-01 21:37:38 +0900

    Add: 問題の解答

---
+++ src/main/fib.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} N
+ * @return {number}
+ */
+const fib = function (N) {
+    let arry = [0, 1];
+
+    for (let i = 2; i <= N; i++) {
+        arry.push(arry[i - 2] + arry[i - 1]);
+    }
+
+    return arry[N];
+};
+module.exports = fib;
