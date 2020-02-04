617ba81e51bfbc47a3a585d1bd7cbae066956b74
onesword0618 <ao.akua.leo@gmail.com>
2020-02-04 22:54:04 +0900

    Add: 問題の解答

---
+++ src/main/findWords.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+const findWords = function (words) {
+
+    const top = /^[qwertyuiop]+$/i;
+    const mid = /^[asdfghjkl]+$/i;
+    const bottom = /^[zxcvbnm]+$/i;
+    const layouts = [top, mid, bottom];
+
+    return words.filter(word => layouts.some(layout => layout.test(word)));
+};
+module.exports = findWords;
