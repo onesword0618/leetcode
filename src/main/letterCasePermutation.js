e7d4fcb05c3a502a6548dea7cacfcf6b49a5a5e7
onesword0618 <ao.akua.leo@gmail.com>
2020-02-08 23:56:51 +0900

    問題を回答する

--- src/main/letterCasePermutation.js
+++ src/main/letterCasePermutation.js
@@ -4,5 +4,24 @@
  */
 const letterCasePermutation = function (S) {
 
+    let result = [];
+
+    function dfs(current, i) {
+
+        if (current.length == S.length) {
+            result.push(current);
+            return;
+        };
+
+        if (S.charAt(i) >= '0' && S.charAt(i) <= '9') {
+            current += S.charAt(i);
+            dfs(current, i + 1);
+        } else {
+            dfs(current + S.charAt(i).toLowerCase(), i + 1);
+            dfs(current + S.charAt(i).toUpperCase(), i + 1);
+        };
+    };
+    dfs('', 0);
+    return result;
 };
 module.exports = letterCasePermutation;
--- src/test/letterCasePermutation.test.js
+++ src/test/letterCasePermutation.test.js
@@ -1,8 +1,19 @@
 const letterCasePermutation = require('./../main/letterCasePermutation');
 
-describe('test概要', () => {
+describe('引数の英字のアッパーキャメルのパターンを生成する。', () => {
 
-    test('テストコメントと期待', () => {
-        expect(letterCasePermutation('abbaca')).toBe('ca');
+    test('a1b2を引数に渡して、a1b2,a1B2,A1b2,A1B2を返却する。', () => {
+        expect(letterCasePermutation('a1b2'))
+            .toEqual(expect.arrayContaining(['a1b2', 'a1B2', 'A1b2', 'A1B2']));
+    });
+
+    test('3z4を引数に渡して、3z4,3Z4を返却する。', () => {
+        expect(letterCasePermutation('3z4'))
+            .toEqual(expect.arrayContaining(['3z4', '3Z4']));
+    });
+
+    test('a1b2を引数に渡して、a1b2,a1B2,A1b2,A1B2を返却する。', () => {
+        expect(letterCasePermutation('12345'))
+            .toEqual(expect.arrayContaining(['12345']));
     });
 });
