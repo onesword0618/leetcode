eb8516f79b0a25629bf8094b7e71cb3e9440f195
onesword0618 <ao.akua.leo@gmail.com>
2020-02-10 00:13:02 +0900

    Add: 問題の回答を行った

---
+++ src/main/smallestRangeI.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number}
+*/
+const smallestRangeI = function (A, K) {
+
+    return Math.max(0, Math.max(...A) - Math.min(...A) - 2 * K);
+};
+module.exports = smallestRangeI;
