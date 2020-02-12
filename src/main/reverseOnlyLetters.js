bfab730fab37b07d74457bf7e540ba966420a260
onesword0618 <ao.akua.leo@gmail.com>
2020-02-13 00:33:00 +0900

    Add: 問題を回答する

---
+++ src/main/reverseOnlyLetters.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} S
+ * @return {string}
+ */
+const reverseOnlyLetters = function (S) {
+
+    let index = 0;
+    let words = S.replace(/[^a-zA-Z]/g, '').split('').reverse();
+    return S.replace(/[a-zA-Z]/g, _ => words[index++]);
+};
+module.exports = reverseOnlyLetters;
