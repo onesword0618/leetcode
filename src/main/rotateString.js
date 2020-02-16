8812ca70226c752c236432f00965be9627c256b0
onesword0618 <ao.akua.leo@gmail.com>
2020-02-16 23:29:03 +0900

    Add: 問題の回答を行う

---
+++ src/main/rotateString.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} A
+ * @param {string} B
+ * @return {boolean}
+ */
+const rotateString = function (A, B) {
+    return (B + B).indexOf(A) != -1 && A.length == B.length
+};
+module.exports = rotateString;
