2c76e1794d2dd70178061b7b7709767da6e4dcd0
onesword0618 <ao.akua.leo@gmail.com>
2020-02-17 22:19:51 +0900

    Add: 問題の回答を行った

---
+++ src/main/findComplement.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+const findComplement = function (num) {
+
+    const str = num.toString(2);
+    let result = '';
+
+    for (let i of str) {
+
+        result += i === '1' ? '0' : '1';
+    };
+    return parseInt(result, 2);
+};
+module.exports = findComplement;
---
+++ src/test/findComplement.test.js
@@ -0,0 +1,12 @@
+const findComplement = require('./../main/findComplement');
+
+describe('正の引数の補数を返却する  ', () => {
+
+    test('5を渡して2が返却される', () => {
+        expect(findComplement(5)).toBe(2);
+    });
+
+    test('1を渡して0が返却される', () => {
+        expect(findComplement(1)).toBe(0);
+    });
+});
