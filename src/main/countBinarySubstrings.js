3fde52a10c1f3ad61064bfe9ec70e036bd0c53c2
onesword0618 <ao.akua.leo@gmail.com>
2020-02-21 00:03:06 +0900

    Add: 問題の回答を行う。

---
+++ src/main/countBinarySubstrings.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const countBinarySubstrings = function(s) {
+    return s.replace(/01/g, '0,1').replace(/10/g, '1,0').split(',')
+        .reduce((res, a, i, arr) => i ? res + Math.min(a.length, arr[--i].length) : 0, 0)
+};
+module.exports = countBinarySubstrings;
