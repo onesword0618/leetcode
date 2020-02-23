c5dd13b732d6ffa91470aad7620a9bc77d0a0000
onesword0618 <ao.akua.leo@gmail.com>
2020-02-23 22:35:09 +0900

    Update: scriptブロックに単一のテストケースの実行を追記した

--- src/test/isValid.test.js
+++ src/test/isValid.test.js
@@ -3,15 +3,15 @@ const isValid = require('./../main/isValid');
 describe('開いたbracketsがあれば、同じタイプのbracketsで閉じているかどうか', () => {
 
     test(' ( ) の入力が正しいこと', () => {
-        expect(isValid('()')).toBeTruthy;
+        expect(isValid('()')).toBeTruthy();
     });
 
     test(' ( ) [] {} の入力が正しいこと', () => {
-        expect(isValid('()[]{}')).toBeTruthy;
+        expect(isValid('()[]{}')).toBeTruthy();
     });
 
     test(' ( ] の入力が不正であること', () => {
-        expect(isValid('(]')).toBeFalsy;
+        expect(isValid('(]')).toBeFalsy();
     });
 
 
