57fbe8399cab1c91706698bfa8b1474e76bdff6d
onesword0618 <ao.akua.leo@gmail.com>
2020-03-26 23:58:06 +0900

    Add: 2つの配列をルールに従って加工した配列を返却する問題を回答した

---
+++ src/main/createTargetArray.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} index
+ * @return {number[]}
+ */
+module.exports = (nums, index) => {
+    let target = [];
+    index.forEach((x, idx) => target.splice(x, 0, nums[idx]));
+    return target;
+};
---
+++ src/test/createTargetArray.test.js
@@ -0,0 +1,26 @@
+const createTargetArray = require('./../main/createTargetArray.js');
+//   Question
+//      Given two arrays of integers nums and index.Your task is to create target array under the following rules:
+//          Initially target array is empty.
+//          From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+//          Repeat the previous step until there are no elements to read in nums and index.
+//      Return the target array.
+//      It is guaranteed that the insertion operations will be valid.
+
+describe('2つの配列を提示されたルールに従った配列を返却する', () => {
+
+    describe('createTargetArray([nums],[index])が実行されたとき', () => {
+
+        test('nums = [0,1,2,3,4], index =[4,1,2,3]の場合、[0,4,1,3,2]が返却されること', () => {
+            expect(createTargetArray([0, 1, 2, 3, 4], [0, 1, 2, 2, 1])).toEqual(expect.arrayContaining([0, 4, 1, 3, 2]));
+        });
+
+        test('nums = [1,2,3,4,0], index = [0,1,2,3,0]の場合、[0,1,2,3,4]が返却されること', () => {
+            expect(createTargetArray([1, 2, 3, 4, 0], [0, 1, 2, 3, 0])).toEqual(expect.arrayContaining([0, 1, 2, 3, 4]));
+        });
+
+        test('nums = [1], index = [0]の場合、[1]が返却されること', () => {
+            expect(createTargetArray([1], [0])).toEqual(expect.arrayContaining([1]));
+        });
+    });
+});
