daf498d61fa8833adb5ca9bf8d1ef5e0c9f2e896
onesword0618 <ao.akua.leo@gmail.com>
2020-03-29 17:36:15 +0900

    Add: 引数の配列の要素を並び替える問題の回答をした

---
+++ src/main/replaceElements.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+module.exports = arr => {
+
+    let currMax = arr[arr.length - 1];
+    arr[arr.length - 1] = -1;
+
+    let length = arr.length - 2;
+
+    for (let i = length; i >= 0; i--) {
+
+        let curr = arr[i];
+        arr[i] = currMax;
+
+        if (curr > currMax) {
+            currMax = curr;
+        };
+    };
+    return arr;
+};
