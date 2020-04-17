5eec42f9fa582760b8d27ea22e0dc4474d44d7b5
onesword0618 <ao.akua.leo@gmail.com>
2020-04-17 23:24:15 +0900

    Add: Excelの列番号を取得する問題を回答した

---
+++ src/main/titleToNumber.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+module.exports = s => {
+    let columnNumber = 0;
+    for (let i = 0; i < s.length; i++) {
+        let char = s[(s.length - 1) - i];
+        columnNumber += Math.pow(26, i) * (char.charCodeAt(0) - 64);
+    };
+    return columnNumber;
+};
