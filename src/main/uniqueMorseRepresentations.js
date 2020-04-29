26017adcc9d0d61ddfb62e4ea1d5cde0b3757c1a
onesword0618 <ao.akua.leo@gmail.com>
2020-04-29 20:28:32 +0900

    Add: モールス符号語の変換数を返却する問題を回答した

---
+++ src/main/uniqueMorseRepresentations.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+module.exports = words => {
+    const result = [];
+    const codes = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."];
+
+    words.forEach(word => {
+        let str = '';
+
+        for (let i = 0; i < word.length; i++) {
+            const code = word[i].charCodeAt(0) - 97;
+            str += codes[code];
+        };
+
+        if (!result.find(v => v === str)) {
+            result.push(str);
+        };
+    });
+    return result.length;
+};
