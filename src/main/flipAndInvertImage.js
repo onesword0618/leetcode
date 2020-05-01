d4e8eaa8c5f25b3146823e690e99b33b09221067
onesword0618 <ao.akua.leo@gmail.com>
2020-05-01 23:07:51 +0900

    Add: 画像を反転させる問題を回答した

---
+++ src/main/flipAndInvertImage.js
@@ -0,0 +1,7 @@
+/**
+ * @param {number[][]} A
+ * @return {number[][]}
+ */
+module.exports = A => {
+    return A.map(row => row.map(val => val ^ 1).reverse());
+};
---
+++ src/test/flipAndInvertImage.test.js
@@ -0,0 +1,43 @@
+const flipAndInvertImage = require('./../main/flipAndInvertImage.js');
+//      Question
+//           Given a binary matrix A, we want to flip the image horizontally,
+//           then invert it, and return the resulting image.
+//           To flip an image horizontally means that each row of the image is reversed.
+//           For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
+//           To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0.
+//           For example, inverting [0, 1, 1] results in [1, 0, 0].
+
+describe('画像を表現したバイナリの配列を反転させた配列を返却する', () => {
+
+    describe('flipAndInvertImage(number[][] A) を実行するとき', () => {
+
+        test('numbers = [[1,1,0],[1,0,1],[0,0,0]]を入力して[[1,0,0],[0,1,0],[1,1,1]]が返却されること', () => {
+            expect(flipAndInvertImage([
+                [1, 1, 0],
+                [1, 0, 1],
+                [0, 0, 0]
+            ])).toEqual(expect.arrayContaining([
+                [1, 0, 0],
+                [0, 1, 0],
+                [1, 1, 1]
+            ]));
+        });
+    });
+
+    describe('flipAndInvertImage(number[][] A) を実行するとき', () => {
+
+        test('numbers = [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]を入力して[[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]が返却されること', () => {
+            expect(flipAndInvertImage([
+                [1, 1, 0, 0],
+                [1, 0, 0, 1],
+                [0, 1, 1, 1],
+                [1, 0, 1, 0]
+            ])).toEqual(expect.arrayContaining([
+                [1, 1, 0, 0],
+                [0, 1, 1, 0],
+                [0, 0, 0, 1],
+                [1, 0, 1, 0]
+            ]));
+        });
+    });
+});
