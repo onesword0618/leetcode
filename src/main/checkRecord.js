3e355e5dc01e602f351b15851f4087173698d8d8
onesword0618 <ao.akua.leo@gmail.com>
2020-05-02 20:51:34 +0900

    Add: 生徒の出席簿に対する判断の問題を回答した

---
+++ src/main/checkRecord.js
@@ -0,0 +1,7 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+module.exports = s => {
+    return s.split('').filter(s => s == 'A').length <= 1 && !s.includes('LLL');
+};
---
+++ src/test/checkRecord.test.js
@@ -0,0 +1,22 @@
+const checkRecord = require('./../main/checkRecord.js');
+//      Question
+//           You are given a string representing an attendance record for a student. The record only contains the following three characters:
+//           'A' : Absent.
+//           'L' : Late.
+//           'P' : Present.
+//           A student could be rewarded if his attendance record doesn't contain more than one 'A' (absent) or more than two continuous 'L' (late).
+//           You need to return whether the student could be rewarded according to his attendance record.
+
+describe('第一引数の文字列に対してあるルールに従って報酬を受け取ることができるかの判定を返却する', () => {
+
+    describe('checkRecord(string s) を実行するとき', () => {
+
+        test('s = PPALLPを入力して真が返却されること', () => {
+            expect(checkRecord('PPALLP')).toBeTruthy();
+        });
+
+        test('s = PPALLLを入力して偽が返却されること', () => {
+            expect(checkRecord('PPALLL')).toBeFalsy();
+        });
+    });
+});
