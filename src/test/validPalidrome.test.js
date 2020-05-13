e6cffbf9d8546738025e68f6ec9ccfcfe717a54a
onesword0618 <ao.akua.leo@gmail.com>
2020-05-13 22:03:11 +0900

    Add: バックスペースの文字列の比較の問題を回答した

--- src/test/validPalidrome.test.js
+++ src/test/validPalidrome.test.js
@@ -11,12 +11,12 @@ describe('引数の文字列が回文になるかどうか判定する', () => {
             test('s = A man, a plan, a canal: Panamaの場合、真であること', () => {
                 expect(validPalindrome('A man, a plan, a canal: Panama')).toBeTruthy();
             });
+        });
 
-            describe('validPalindrome(nums)が偽の場合', () => {
+        describe('validPalindrome(nums)が偽の場合', () => {
 
-                test('s = race a carの場合、偽である    こと', () => {
-                    expect(validPalindrome('race a car')).toBeFalsy();
-                });
+            test('s = race a carの場合、偽である    こと', () => {
+                expect(validPalindrome('race a car')).toBeFalsy();
             });
         });
     });
