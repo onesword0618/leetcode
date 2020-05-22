c6160c78b9be05b4ff653acc31344601cdb60005
onesword0618 <ao.akua.leo@gmail.com>
2020-05-22 23:53:50 +0900

    Add: 壊れた要素を復元する問題を回答した

---
+++ src/main/findErrorNums.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+module.exports = nums => {
+
+    let x, y;
+    let sum = 0;
+    let times = new Array(nums.length).fill(0);
+
+    for (let n of nums) {
+        sum += n;
+        times[n - 1]++;
+
+        if (times[n - 1] === 2) {
+            x = n;
+        };
+    };
+
+    y = (nums.length + 1) * nums.length / 2 + x - sum;
+    return [x, y];
+
+};
