ec70c3d7100a85ba95319542dbf55d5ec0902487
onesword0618 <ao.akua.leo@gmail.com>
2020-06-04 22:37:00 +0900

    Add: 与えられた幅に対して文字列を書き込んだときの行数と最終行の幅の問題を回答した

---
+++ src/main/numberOfLines.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} widths
+ * @param {string} S
+ * @return {number[]}
+ */
+module.exports = (widths, S) => {
+    return [...S]
+        .map(c => widths[c.charCodeAt(0) - 97])
+        .reduce(
+            (acc, curr) => [
+                acc[0] + (100 < acc[1] + curr ? 1 : 0),
+                100 < acc[1] + curr ? curr : acc[1] + curr
+            ],
+            [S.length ? 1 : 0, 0]
+        );
+};
