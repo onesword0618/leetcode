e2dce0b042268f719760a31bc35d59226a97cd74
onesword0618 <ao.akua.leo@gmail.com>
2020-06-12 22:07:23 +0900

    Add: 与えられた引数の文字列の順番を入れ替えても等しくなるかどうかの問題を回答した.

---
+++ src/main/buddyStrings.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} A
+ * @param {string} B
+ * @return {boolean}
+ */
+module.exports = (A, B) => {
+    if (A == "" || B == "" || A.length !== B.length) {
+        return false;
+    };
+
+    if (A == B) {
+        let set = new Set(A);
+        return set.size !== A.length;
+    };
+
+    let a = "", b = "";
+
+    for (i = 0; i < A.length; i++) {
+        if (A[i] !== B[i]) {
+            a += A[i];
+            b += B[i];
+        };
+    };
+
+    if (a.length == 2 && a.length == b.length) {
+        return a[0] == b[1] && a[1] == b[0];
+    };
+    return false;
+};
