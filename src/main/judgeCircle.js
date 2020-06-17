9d3482eb8db1c27e0617c6f38ec12a5fd2af80cf
onesword0618 <ao.akua.leo@gmail.com>
2020-06-17 23:47:11 +0900

    Add: ロボットの動きを配列で表現してもとの位置戻ることができるかの問題を回答した

---
+++ src/main/judgeCircle.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+module.exports = moves => {
+    return moves.split('')
+        .reduce((p, m) => [p[0] + (m === 'R') - (m === 'L'), p[1] + (m === 'U') - (m === 'D')], [0, 0])
+        .join('') === '00';
+};
