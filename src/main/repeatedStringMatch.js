9ffdd5735783f03412335517719cd0997bcd5318
onesword0618 <ao.akua.leo@gmail.com>
2020-06-22 23:37:05 +0900

    Add:2つの文字列で1つの文字列の部分文字列になる最小値を返却する

---
+++ src/main/repeatedStringMatch.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} A
+ * @param {string} B
+ * @return {number}
+ */
+module.exports = (A, B) => {
+    let n = Math.ceil(B.length / A.length);
+    return A
+        .repeat(n)
+        .includes(B)
+        || A
+            .repeat(++n)
+            .includes(B)
+        || A
+            .repeat(++n)
+            .includes(B)
+        ? n : -1;
+};
