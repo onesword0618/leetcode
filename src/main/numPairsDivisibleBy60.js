43d4b2f18f2841100ba637f38202cfe0993d0d56
onesword0618 <ao.akua.leo@gmail.com>
2020-06-28 21:26:25 +0900

    Add: 曲時間の合計を60で割れるペアの数を返却する問題を回答した

---
+++ src/main/numPairsDivisibleBy60.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} time
+ * @return {number}
+ */
+module.exports = time => {
+    let count = 0;
+    let map = new Map();
+
+    for (let num of time) {
+        let mod = num % 60;
+        let sHalf = mod === 0 ? 0 : 60 - mod;
+
+        if (map.has(sHalf)) {
+            count += map.get(sHalf);
+        };
+
+        if (map.has(mod)) {
+            map.set(mod, map.get(mod) + 1);
+        } else {
+            map.set(mod, 1);
+        };
+    };
+    return count;
+};
