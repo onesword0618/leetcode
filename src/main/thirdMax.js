c9edbf2620f0fe0d79eaa44514b4e59df1009e15
onesword0618 <ao.akua.leo@gmail.com>
2020-07-19 22:32:26 +0900

    Add: 3番目の最大値を返却する

---
+++ src/main/thirdMax.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+module.exports = thirdMax = function (nums) {
+
+    let arr = Array.from(new Set(nums));
+
+    if (arr.length < 3) {
+        return Math.max(...arr);
+    };
+
+    arr.splice(arr.indexOf(Math.max(...arr)), 1);
+    arr.splice(arr.indexOf(Math.max(...arr)), 1);
+    return Math.max(...arr);
+};
---
+++ src/test/thirdMax.test.js
@@ -0,0 +1,15 @@
+const thirdMax = require('./../main/thirdMax.js');
+//    Question
+//       Given a non-empty array of integers, return the third maximum number in this array. If it does not exist, return the maximum number. The time complexity must be in O(n).
+
+describe('3番目の大きさの数の要素を返却する', () => {
+    describe('thirdMax(number[] nums)  を実行するとき', () => {
+        test('number[] = [3, 2, 1]を引数に1が返却されること', () => {
+            expect(thirdMax([3, 2, 1])).toBe(1);
+        });
+
+        test('number[] = [1, 2]を引数に2が返却されること', () => {
+            expect(thirdMax([1, 2])).toBe(2);
+        });
+    });
+});
