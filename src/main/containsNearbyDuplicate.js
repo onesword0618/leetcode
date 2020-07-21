aca67ce6918e962d7d432cc98a225696999a22f5
onesword0618 <ao.akua.leo@gmail.com>
2020-07-22 00:41:39 +0900

    Add: 絶対差の最大値を構成できる配列の要素があるかを判断する問題を回答

---
+++ src/main/containsNearbyDuplicate.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+module.exports = (nums, k) => {
+    let m = new Map();
+    for (i = 0; i < nums.length; i++) {
+        if (!m.has(nums[i])) m.set(nums[i], i);
+        else {
+            if (i - m.get(nums[i]) <= k) return true;
+            else m.set(nums[i], i);
+        }
+    }
+    return false;
+};
