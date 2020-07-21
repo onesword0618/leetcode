b78f854fab5d80ddb040169ef3e1cf9b5846ff29
onesword0618 <ao.akua.leo@gmail.com>
2020-07-22 00:42:52 +0900

    Fix: コメントの修正をした

--- src/test/containsNearbyDuplicate.test.js
+++ src/test/containsNearbyDuplicate.test.js
@@ -1,7 +1,7 @@
 const containsNearbyDuplicate = require('./../main/containsNearbyDuplicate.js');
 //    Question
 //   Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
-describe('絶対差の最大値を構成できる入れるの要素があるかを判断して返却する', () => {
+describe('絶対差の最大値を構成できる配列の要素があるかを判断して返却する', () => {
 
     describe('containsNearbyDuplicate(string s)  を実行するとき', () => {
         test('nums = [1,2,3,1], k = 3を入力して真を返却すること', () => {
