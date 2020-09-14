05c445088041aa4518aa49a28c4fc411fdd5e513
onesword0618 <ao.akua.leo@gmail.com>
2020-09-14 23:58:14 +0900

    Add: 対角線の和を求める問題を回答した

---
+++ src/main/diagonalSum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+module.exports = diagonalSum = function (mat) {
+    let sum = 0;
+
+    for (let i = 0; i < mat.length; i++) {
+        sum += mat[i][i];
+
+        if (i !== mat[i].length - i - 1) {
+            sum += mat[i][mat[i].length - i - 1];
+        }
+    }
+
+    return sum;
+};
---
+++ src/test/diagonalSum.test.js
@@ -0,0 +1,15 @@
+const diagonalSum = require('./../main/diagonalSum');
+//   Question
+//      Given a square matrix mat, return the sum of the matrix diagonals.
+//      Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+
+describe('渡した引数に対角行の合計を返却する', () => {
+
+    test('[[1,2,3],[4,5,6],[7,8,9]]の引数を渡して25が返却される', () => {
+        expect(diagonalSum([[1, 2, 3], [4, 5, 6], [7, 8, 9]])).toBe(25);
+    });
+
+    test('[[ 5 ]]の引数を渡して5が返却される', () => {
+        expect(diagonalSum([[5]])).toBe(5);
+    });
+});
