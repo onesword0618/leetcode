544b8b268470c7d6246d5b0f3f74e460fbd84cdb
onesword0618 <ao.akua.leo@gmail.com>
2020-09-25 23:13:18 +0900

    Add: 配列のラッキーナンバーを求める問題を回答する

---
+++ src/main/luckyNumbers.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+module.exports = luckyNumbers = function (matrix) {
+    const luckey = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+        const x = matrix[i].indexOf(Math.min(...matrix[i]));
+        if (x === -1) {
+            continue;
+        }
+
+        let flag = true;
+
+        for (let j = 0; j < matrix.length; j++) {
+            if (j == i) {
+                continue;
+            }
+
+            if (x <= matrix[j].length) {
+                if (matrix[i][x] < matrix[j][x]) {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+
+        if (flag) {
+            luckey.push(matrix[i][x]);
+        }
+    }
+    return luckey;
+};
