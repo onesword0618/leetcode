ee3da1f635ec5046790f95a7761f8dfa7afb4a2c
onesword0618 <ao.akua.leo@gmail.com>
2020-10-01 00:01:40 +0900

    Add: 配列の引数でラッキー整数を回答する

---
+++ src/main/findLucky.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+module.exports = findLucky = function (arr) {
+    const map = {};
+    let max = -1;
+
+    for (let i = 0; i < arr.length; i++) {
+        map[arr[i]] ? map[arr[i]]++ : map[arr[i]] = 1;
+    }
+
+    for (let el in map) {
+        if (Number(el) === map[el] && Number(el) > max) max = Number(el);
+    }
+    return max;
+};
