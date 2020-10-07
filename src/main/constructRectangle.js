2bd271193cfd91841c7637df3927b7ece4785737
onesword0618 <ao.akua.leo@gmail.com>
2020-10-07 23:19:11 +0900

    Add: 引数の数字を元に長方形の縦・横の長さの配列を回答した

---
+++ src/main/constructRectangle.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} area
+ * @return {number[]}
+ */
+module.exports = constructRectangle = function (area) {
+    let width = Math.floor(Math.sqrt(area));
+
+    while (width > 0) {
+        const length = area / width
+        if (Number.isInteger(length)) {
+            return [length, width]
+        }
+        width--
+    }
+};
