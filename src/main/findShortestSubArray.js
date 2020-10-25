1c4c6b232590a8e3e13a282e552bfd7eda47346b
onesword0618 <ao.akua.leo@gmail.com>
2020-10-25 19:33:44 +0900

    Add: 配列の次数を返却するように回答した。

---
+++ src/main/findShortestSubArray.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+module.exports = nums => {
+    let m = new Map(), left = new Map(), right = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i];
+        if (left.get(num) === undefined) left.set(num, i);
+        right.set(num, i);
+        m.set(num, m.get(num) + 1 || 1);
+    }
+
+    let degree = Math.max(...Array.from(m.values()));
+    let elems = Array.from(m.keys()).filter(num => m.get(num) === degree);
+    let len = nums.length;
+
+    for (let elem of elems) {
+        len = Math.min(len, right.get(elem) - left.get(elem) + 1);
+    }
+    return len;
+};
