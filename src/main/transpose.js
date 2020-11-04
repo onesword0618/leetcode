08611382a5ce2d107f2a5a34d42d5902fd7dbfe8
onesword0618 <ao.akua.leo@gmail.com>
2020-11-04 23:56:23 +0900

    Add: 表の行と列を入れ替える

---
+++ src/main/transpose.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} A
+ * @return {number[][]}
+ */
+module.exports = A => {
+    let result = [];
+    let size = A.length;
+    let innerSize = A[0].length;
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < innerSize; j++) {
+            if (result[j] == null) {
+                result[j] = [];
+            }
+            result[j][i] = A[i][j];
+        }
+    }
+    return result;
+};
