d59f0b76283d36af903acc8008bd1ccadac7f6a2
onesword0618 <ao.akua.leo@gmail.com>
2021-01-01 22:32:00 +0900

    Add: 与式で与えられた文字列を分割したときに類似かどうか
    特に感慨はない。
    JestをJavaScriptで書くのは慣れたので次はその先を実施しないといけない。

---
+++ src/test/halvesAreAlike.test.js
@@ -0,0 +1,24 @@
+const halvesAreAlike = require('./../main/halvesAreAlike');
+//   Question
+//      You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
+//      Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
+//      Return true if a and b are alike. Otherwise, return false.
+
+describe('与えられた文字列を半分にした場合に類似かどうかを返却する', () => {
+
+    test('bookを渡してtrueが返却される', () => {
+        expect(halvesAreAlike('book')).toBeTruthy;
+    });
+
+    test('textbookを渡してfalseが返却される', () => {
+        expect(halvesAreAlike('textbook')).toBeFalsy;
+    });
+
+   test('MerryChristmasを渡してfalseが返却される',() => {
+        expect(halvesAreAlike('MerryChristmas')).toBeFalsy;
+   });
+
+   test('AbCdEfGhを渡してtrueが返却される',() => {
+       expect(halvesAreAlike('AbCdEfGh')).toBeTruthy;
+   });
+});
