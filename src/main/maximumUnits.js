e5863a0b6172c4e3eee3ede31314b8e990e498ba
onesword0618 <ao.akua.leo@gmail.com>
2021-01-03 23:14:45 +0900

    Add: トラックの積載量について回答した

---
+++ src/main/maximumUnits.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} boxTypes
+ * @param {number} truckSize
+ * @return {number}
+ */
+module.exports = maximumUnits = function(boxTypes, truckSize) {
+   boxTypes.sort((a, b) => b[1] - a[1]);
+   let ans = 0;
+   for (const [count, units] of boxTypes) {
+     const takeBoxes = Math.min(count, truckSize);
+     ans += takeBoxes * units;
+     truckSize -= takeBoxes;
+     if (truckSize === 0) {
+      break;
+     }
+    }
+    return ans;
+};
