71c52bffb0a19b4549fdaf76293692b57edbe80c
onesword0618 <ao.akua.leo@gmail.com>
2021-01-21 23:56:23 +0900

    Add: 配列の並び替えの問題を回答した

---
+++ src/main/summaryRanges.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+module.exports = summaryRanges = function(nums) {
+ let i = 0, res = [];
+ for (let j = 0; j < nums.length; j++) {
+     if (nums[j+1] === nums[j] + 1) continue;
+     else {
+         if (i == j) res.push(nums[i] + "");
+         else res.push( nums[i] + "->" + nums[j] );
+         i = j + 1;
+     }
+ }
+ return res;
+};
