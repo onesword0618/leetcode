8c816cca6187b757568ed52cb0429ebee860aa75
onesword0618 <ao.akua.leo@gmail.com>
2021-03-15 23:40:46 +0900

    Add: 重ねっている図形の総面積を回答した

---
+++ src/main/computeArea.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} A
+ * @param {number} B
+ * @param {number} C
+ * @param {number} D
+ * @param {number} E
+ * @param {number} F
+ * @param {number} G
+ * @param {number} H
+ * @return {number}
+ */
+ module.exports = computeArea = (A, B, C, D, E, F, G, H) => {
+  let area = (C - A) * (D - B) + (G - E) * (H - F);
+  if (!(C <= E || D <= F || G <= A || H <= B)) {
+      area -= (Math.min(C, G) - Math.max(E, A)) * (Math.min(D, H) - Math.max(F, B));
+  };
+  return area;
+};
