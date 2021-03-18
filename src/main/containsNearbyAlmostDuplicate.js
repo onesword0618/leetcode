2793d76b567e1218b6747a1d3e2cb2b5d36b814c
onesword0618 <ao.akua.leo@gmail.com>
2021-03-18 23:23:32 +0900

    Add: 配列の重複について判断して回答する

---
+++ src/main/containsNearbyAlmostDuplicate.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} t
+ * @return {boolean}
+ */
+ module.exports = containsNearbyAlmostDuplicate = (nums, k, t) => {
+  for (i = 0; i <= nums.length; i++) {
+      for (j = i+1; j <= i+k; j++) {
+          if (Math.abs(nums[i]-nums[j]) <= t) return true;
+      }
+  }
+  return false;
+};
