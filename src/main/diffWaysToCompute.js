d055d3cd4a425203b5a1d7ec40c4d7f4ff3e3f3c
onesword0618 <ao.akua.leo@gmail.com>
2021-03-27 00:15:36 +0900

    Add: 文字列に対して括弧を追加して計算した結果を配列で回答した

---
+++ src/main/diffWaysToCompute.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} expression
+ * @return {number[]}
+ */
+ module.exports = diffWaysToCompute = function(expression) {
+  let res = [];
+  for (let i = 0; i < expression.length; i++) {
+      if (isNaN(expression[i])) {
+          let left = diffWaysToCompute(expression.slice(0, i));
+          let right = diffWaysToCompute(expression.slice(i+1));
+          for (let l of left) {
+              for (let r of right) {
+                  l = Number(l);
+                  r = Number(r);
+
+                  if (expression[i]=='+') {
+                      res.push(l + r);
+                  } else if (expression[i]=='-') {
+                      res.push(l - r);
+                  } else {
+                      res.push(l * r);
+                  }
+
+              }
+          }
+      }
+  }
+
+  if (res.length!=0) return res;
+  return [expression];
+};
