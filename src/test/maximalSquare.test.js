2be8e49b562b36d8d88fd82fd45eb4f0512266d1
onesword0618 <ao.akua.leo@gmail.com>
2021-03-28 01:06:57 +0900

    Add: 最大の四角の大きさを回答した

---
+++ src/test/maximalSquare.test.js
@@ -0,0 +1,21 @@
+const maximalSquare = require('./../main/maximalSquare');
+//      Question
+//        Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+
+describe('最大の四角の大きさの数を返却する', () => {
+
+    describe('maximalSquare(character[][] matrix) を実行するとき', () => {
+
+        test('matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]を入力して19が返却されること', () => {
+            expect(maximalSquare([["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]])).toBe(4);
+        });
+
+        test('matrix =  [["0","1"],["1","0"]]を入力して19が返却されること', () => {
+            expect(maximalSquare([["0","1"],["1","0"]])).toBe(1);
+        });
+
+        test('matrix = [["0"]]を入力して19が返却されること', () => {
+            expect(maximalSquare([["0"]])).toBe(0);
+        });
+    });
+});
