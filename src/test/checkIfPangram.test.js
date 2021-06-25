ae56e4b91d0ee1a86d1a133ebc67db8515ab00a1
onesword0618 <ao.akua.leo@gmail.com>
2021-06-26 00:42:02 +0900

    Add: パングラムの問題を回答した

---
+++ src/test/checkIfPangram.test.js
@@ -0,0 +1,16 @@
+const checkIfPangram = require('./../main/checkIfPangram');
+//   Question
+//      A pangram is a sentence where every letter of the English alphabet appears at least once.
+//      Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+describe('アルファベットが全て含まれているかどうかの結果を返却する', () => {
+ describe('checkIfPangram (string sentence) を実行するとき', () => {
+  test('sentence = thequickbrownfoxjumpsoverthelazydog を入力して true が返却されること', () => {
+   expect(checkIfPangram(`thequickbrownfoxjumpsoverthelazydog`)).toBeTruthy();
+  });
+
+  test('sentence = leetcode を入力して false が返却されること', () => {
+   expect(checkIfPangram(`leetcode`)).toBeFalsy();
+  });
+ });
+});
