53678cc8efd560e607e9d6dc6895702b391ea02b
onesword0618 <ao.akua.leo@gmail.com>
2021-06-29 23:46:49 +0900

    Add: 交差点の問題を回答した

---
+++ src/main/intersect.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+module.exports = intersect = function(nums1, nums2) {
+  let checkArray = nums1
+  let checkerArray = nums2;
+
+  return checkArray.filter(
+      a => {
+          let i = checkerArray.indexOf(a);
+          checkerArray[i] = -1;
+          return i<0 ? false: true;
+      }
+  );
+};
---
+++ src/test/intersect.test.js
@@ -0,0 +1,20 @@
+const intersect = require('./../main/intersect');
+//   Question
+//      Given two integer arrays nums1 and nums2,
+//      return an array of their intersection.
+//      Each element in the result must appear as many times as it shows in both
+//      arrays and you may return the result in any order.
+
+describe('2つの配列の共通の要素の配列の結果を返却する', () => {
+   describe('intersect (number[] nums1, number[] nums2) を実行するとき', () => {
+      test('nums1 = [1,2,2,1], nums2 = [2,2] を入力して [2,2] が返却されること', () => {
+         expect(intersect([1,2,2,1],[2,2])).toEqual(expect.arrayContaining([2,2]));
+      });
+   });
+
+   describe('intersect (number[] nums1, number[] nums2) を実行するとき', () => {
+    test('nums1 = [4,9,5], nums2 = [9,4,9,8,4] を入力して [4,9] が返却されること', () => {
+       expect(intersect([4,9,5],[9,4,9,8,4])).toEqual(expect.arrayContaining([4,9]));
+    });
+ });
+});
